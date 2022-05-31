87c42646b7dd108b89fb9bdb30545bb4cc4a25b8
LiWei <2622695020@qq.com>
2022-05-31 20:59:30 +0800

    本次修改

---
+++ room-manager/src/views/Login.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="Login">
+	  <div id="top">
+	  	  <el-radio-group v-model="labelPosition" size="small">
+		  <el-button label="left"> 登录</el-button>
+	      <el-button label="right" @click ="tiaozhuanRegister()" style="margin-left: 35px;">注册</el-button>
+	  </el-radio-group>
+	  </div>
+	  <div id="content">
+	  	<el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+<!-- 	    <el-form-item label="用户名" prop="userName">
+	      <el-input type="text" v-model="ruleForm.userName" autocomplete="off"></el-input>
+	    </el-form-item> -->
+	    <el-form-item label="手机号" prop="phone">
+	      <el-input type="number" v-model="ruleForm.phone" autocomplete="off"></el-input>
+	    </el-form-item>
+	    <el-form-item label="密码" prop="password">
+	      <el-input type="password" v-model="ruleForm.password"></el-input>
+	    </el-form-item>
+	    <div id="btnRegister">
+	    	<el-form-item>
+	    	  <el-button type="primary" style="width: 150px;" @click="submitForm('ruleForm')"  >登录</el-button>
+	    	  <!-- <el-button style="margin-left: 50px;"  @click="resetForm('ruleForm')"  >重置</el-button> -->
+	    	</el-form-item>
+	    </div>
+	  </el-form>
+	  </div>
+	  
+  </div>
+</template>
+
+<script>
+	
+	export default{
+		el: "Login",
+		    data() {
+				// var checkName = (rule, value, callback) => {
+				// 	  if (value === '') {
+				// 		callback(new Error('请输入用户名'));
+				// 	  } else {
+				// 		if (this.ruleForm.userName !== '') {
+				// 		  this.$refs.ruleForm.validateField('userName');
+				// 		}
+				// 		callback();
+				// 	  }
+				// 	};
+					var checkPhone = (rule, value, callback) => {
+					  if (value === '') {
+						callback(new Error('请输入手机号'));
+					  } else {
+						if (this.ruleForm.phone !== '') {
+						  this.$refs.ruleForm.validateField('phone');
+						}
+						callback();
+					  }
+					};
+					
+					var checkPassword = (rule, value, callback) => {
+					  if (value === '') {
+						callback(new Error('请输入密码'));
+					  } else {
+						if (this.ruleForm.password !== '') {
+						  this.$refs.ruleForm.validateField('password');
+						}
+						callback();
+					  }
+					};	
+
+		      return {
+		        labelPosition: 'left',
+				 ruleForm: {
+				          userName: '',
+				          phone: '',
+				          password: ''
+				        },
+				        rules: {
+				          // userName: [
+				          //   { validator: checkName, trigger: 'blur' }
+				          // ],
+				          phone: [
+				            { validator: checkPhone, trigger: 'blur' }
+				          ],
+				          password: [
+				            { validator: checkPassword, trigger: 'blur' }
+				          ]
+				        }
+				      }
+			       },
+		   
+			  
+			   methods: {
+			        submitForm(formName) {
+			          this.$refs[formName].validate((valid) => {
+			            if (valid) {
+			              alert('submit!');
+			            } else {
+			              console.log('error submit!!');
+			              return false;
+			            }
+			          });
+			        },
+					
+					tiaozhuanRegister(){
+						this.$router.push("/Register");
+					}
+			      }
+	}		  
+</script>
+
+<style type="text/css">
+	#Login{
+		width: 500px ;
+		height: 340px;
+		margin:  0 auto;
+		background-color: skyblue;
+		border-radius: 5%;
+	}
+	#top{
+		width: 100%;
+		height: 50px;
+		padding-left: 150px;
+		margin: 20px 0;
+	}
+	#content{
+		width: 400px;
+		font-weight: bold;
+	}
+	#btnRegister{
+	width: 500px;
+	padding: 0 50px;
+	/* margin-top: 30px; */
+	margin: 20px 0;
+	}
+	
+</style>
--- room-manager/src/views/MainView.vue
+++ room-manager/src/views/MainView.vue
@@ -1,8 +1,65 @@
 <template>
+	<el-container>
+		<!-- 头部导航栏 -->
+	  <el-header>
+		  <div id="myhead_content">
+		  	<span>会议室预约管理系统</span>
+		  </div>
+	  </el-header>
+	  
+	  <!-- 侧边栏 -->
+	  <el-container>
+	    <el-aside width="200px">Aside</el-aside>
+		
+		<!-- 主页面 -->
+	    <el-main>Main</el-main>
+	  </el-container>
+	  
+	  
+	</el-container>
 </template>
 
 <script>
 </script>
 
 <style>
+	html,body{
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		
+	}
+	 .el-header, .el-footer {
+	    background-color: #B3C0D1;
+	    color: #333;
+	    text-align: center;
+	    line-height: 60px;
+	  }
+	  
+	  .el-aside {
+	    background-color: #D3DCE6;
+	    color: #333;
+	    text-align: center;
+	    line-height: 200px;
+	  }
+	  
+	  .el-main {
+	    background-color: #E9EEF3;
+	    color: #333;
+	    text-align: center;
+	    line-height: 160px;
+	  }
+	  
+	  body > .el-container {
+	    margin-bottom: 40px;
+	  }
+	  
+	  .el-container:nth-child(5) .el-aside,
+	  .el-container:nth-child(6) .el-aside {
+	    line-height: 260px;
+	  }
+	  
+	  .el-container:nth-child(7) .el-aside {
+	    line-height: 320px;
+	  }
 </style>
